9af106e65ed00d8803f9122a0ecb290b94a4e15d
BT Turner <ben.turner@education.wa.edu.au>
2026-09-13 19:39:14 +0800

    Wire in Power Automate flow endpoints for teacher and student dashboard

--- teacher/dashboard-config.js
+++ teacher/dashboard-config.js
@@ -5,15 +5,15 @@ const existingFlowEndpoints = window.WA_GOLD_RUSH_DASHBOARD_CONFIG.flowEndpoints
 window.WA_GOLD_RUSH_DASHBOARD_CONFIG.flowEndpoints = {
     ...existingFlowEndpoints,
     loginTeacher: existingFlowEndpoints.loginTeacher
-        || 'https://224cde437d52e44da36161836e53cf.cc.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/10/workflows/63479c7d2c794888b481e3d477f60136/triggers/manual/paths/invoke',
+        || 'https://224cde437d52e44da36161836e53cf.cc.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/10/workflows/63479c7d2c794888b481e3d477f60136/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=eg2dklPqD9lEhEmrhI5d6UCRC-0o4Sef-P54qF4wK0M',
     loginStudent: existingFlowEndpoints.loginStudent
-        || 'https://REPLACE-WITH-GGR_LoginStudent-URL',
+        || 'https://224cde437d52e44da36161836e53cf.cc.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/08/workflows/d9e728c9ccdf41ef8bd6b0e2e43a6c30/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=MXdl5PkkbZctyqlB4SCCgPNSutquUXN_MjIQmFzS9cc',
     upsertStudentProfile: existingFlowEndpoints.upsertStudentProfile
-        || 'https://REPLACE-WITH-GGR_UpsertStudentProfile-URL',
+        || 'https://224cde437d52e44da36161836e53cf.cc.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/06/workflows/441ec597942642278c09e29b5c7195cf/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=3VrGGK2ZaELVnG_rtkpS8RT7TGttvKNeb1YeuocZxjI',
     changeStudentPin: existingFlowEndpoints.changeStudentPin
-        || 'https://REPLACE-WITH-GGR_ChangeStudentPin-URL',
+        || 'https://224cde437d52e44da36161836e53cf.cc.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/02/workflows/00d7ad1f20974b5d8a0337e9ea4e9958/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=RpwToQQqWyJTJzTkGxQ3pGwD6r8C3kL_wWNJwChHx20',
     saveProgress: existingFlowEndpoints.saveProgress
-        || 'https://REPLACE-WITH-GGR_SaveProgress-URL',
+        || 'https://224cde437d52e44da36161836e53cf.cc.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/11/workflows/de843a7b9cd74079ae14dc3b96e2128a/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=UrSe8pJ17rOw8j3vf-TfOQMVowucgt1-FIdZzUa2vCA',
     teacherUnlockStudent: existingFlowEndpoints.teacherUnlockStudent
         || 'https://REPLACE-WITH-GGR_TeacherUnlockStudent-URL',
     teacherResetStudentPin: existingFlowEndpoints.teacherResetStudentPin
